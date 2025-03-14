6317c3937d01b57c364bedd2ec516e51c0a21b49
Piergiorgio Pietra <piergipietra@gmail.com>
2025-03-14 15:55:00 +0100

    added integration with edge config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-
-
+import Button from "@components/Button";
 export default function Home() {
   return (
-    <div>
-      <h1>SITO DEL MATRIMONIO DI ERIKA E PIERGI</h1>
-      
+    <div className="flex flex-col items-center">
+      <h1 className="m-10 text-5xl">SITO DEL MATRIMONIO DI ERIKA E PIERGI</h1>
+      <div>
+        <img src="partecipazione.png" alt="Partecipazione del matrimonio"></img>
+      </div>
+      <Button></Button>
     </div>
   );
 }
---
+++ services/edgeConfigService.ts
@@ -0,0 +1,30 @@
+import { createClient, EdgeConfigClient } from "@vercel/edge-config";
+
+if (!process.env.EDGE_CONFIG) {
+  throw new Error("Missing EDGE_CONFIG environment variable");
+}
+
+class EdgeConfigService {
+  private client: EdgeConfigClient;
+
+  constructor() {
+    this.client = createClient(process.env.EDGE_CONFIG);
+  }
+
+  async getValue<T>(key: string): Promise<T> {
+    try {
+      return await this.client.get<T>(key);
+    } catch (error) {
+      console.error(`Error fetching Edge Config value for key: ${key}`, error);
+      return null;
+    }
+  }
+
+  async hasKey(key: string): Promise<boolean> {
+    const value = await this.getValue(key);
+    return value !== null;
+  }
+}
+
+// Export a **singleton** instance
+export const edgeConfigService = new EdgeConfigService();
